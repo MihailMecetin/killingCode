c1a20f169c2de37cf5f6bed6776c3ecf432aa328
Mihail Mechetin <mihailmecetin@gmail.com>
2019-01-06 18:19:11 +0200

    Add ex5 Study Drills

--- ex5.py
+++ ex5.py
@@ -1,18 +1,18 @@
-my_name = 'Someguy'
-my_age = 23
-my_height = 190 # Centimeters
-my_weight = 90 # kg
-my_eyes = 'Green'
-my_teeth = 'White'
-my_hair = 'Black'
-my_pronoun = 'He'
-my_pronoun2 = 'His'
+Name = 'Someguy'
+Age = 23
+Height = 190 # Centimeters
+Weight = 90 # kg
+Eyes = 'Green'
+Teeth = 'White'
+Hair = 'Black'
+Pronoun = 'He'
+Pronoun2 = 'His'
 
-print(f"Let'stalk about {my_name}.")
-print(f"{my_pronoun}'s {my_height} centimiters tall.")
-print(f"{my_pronoun}'s {my_weight} kilograms heavy.")
+print(f"Let'stalk about {Name}.")
+print(f"{Pronoun}'s {Height} centimiters tall. Which is", round(Height * 0.3937), "in inches." )
+print(f"{Pronoun}'s {Weight} kilograms heavy. Which is", round(Weight * 2.205), "in pounds.")
 print("Actually that's not too much.")
-print(f"{my_pronoun}'s got {my_eyes} eyes and {my_hair} hair.")
-print(f"{my_pronoun2} teeth are usually {my_teeth} depending on the coffee.")
+print(f"{Pronoun}'s got {Eyes} eyes and {Hair} hair.")
+print(f"{Pronoun2} teeth are usually {Teeth} depending on the coffee.")
 
 
