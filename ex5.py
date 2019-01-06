51ac8048b890e1c2d9a467a89fcc379667493fff
Mihail Mechetin <mihailmecetin@gmail.com>
2019-01-06 18:05:51 +0200

    Add ex5, added pronoun variable to change between male/female

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Someguy'
+my_age = 23
+my_height = 190 # Centimeters
+my_weight = 90 # kg
+my_eyes = 'Green'
+my_teeth = 'White'
+my_hair = 'Black'
+my_pronoun = 'He'
+my_pronoun2 = 'His'
+
+print(f"Let'stalk about {my_name}.")
+print(f"{my_pronoun}'s {my_height} centimiters tall.")
+print(f"{my_pronoun}'s {my_weight} kilograms heavy.")
+print("Actually that's not too much.")
+print(f"{my_pronoun}'s got {my_eyes} eyes and {my_hair} hair.")
+print(f"{my_pronoun2} teeth are usually {my_teeth} depending on the coffee.")
+
+
